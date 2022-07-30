418add05fd731f3c2f40c92905eb067d4978e7d3
cistuff <89096213+cistuff@users.noreply.github.com>
2022-07-30 15:14:18 -0400

    Create Scripts.lua

---
+++ Scripts.lua
@@ -0,0 +1,9 @@
+--[[
+
+Max Hub: loadstring(game:HttpGet("https://raw.githubusercontent.com/cistuff/MAXHUB/main/MAXHUB.lua", true))()
+
+Natural Disaster Survival: Unreleased.
+
+JToH: loadstring(game:HttpGet("https://raw.githubusercontent.com/cistuff/MAXHUB/main/Max%20Hub%20Custom/Jtoh.lua", true))()
+
+Obby that gets harder everytime someone wins: Unreleased.
