c3b44b3a1842111af594bd5d86b3ea045478361b
cistuff <89096213+cistuff@users.noreply.github.com>
2022-07-31 10:57:15 -0400

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -2,8 +2,8 @@
 
 Max Hub: loadstring(game:HttpGet("https://raw.githubusercontent.com/cistuff/MAXHUB/main/MAXHUB.lua", true))()
 
-Natural Disaster Survival: Unreleased.
+Juke's Towers of Hell: loadstring(game:HttpGet("https://raw.githubusercontent.com/cistuff/MAXHUB/main/Max%20Hub%20Custom/Jtoh.lua", true))()
 
-JToH: loadstring(game:HttpGet("https://raw.githubusercontent.com/cistuff/MAXHUB/main/Max%20Hub%20Custom/Jtoh.lua", true))()
+Natural Disaster Survival: Unreleased.
 
 Obby that gets harder everytime someone wins: Unreleased.
