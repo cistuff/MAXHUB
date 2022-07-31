db5b6283a6e47253b695b4d9790b2df7382f99c0
cistuff <89096213+cistuff@users.noreply.github.com>
2022-07-31 10:57:53 -0400

    Update MAXHUB.lua

--- MAXHUB.lua
+++ MAXHUB.lua
@@ -10,4 +10,4 @@
 Max Hub is not ready yet! The individual scripts for the games that Max Hub support are located in the Max Hub Custom folder in this repo. 
 
 ]]
-print("Max Hub is not released yet.")
+print("This version of Max Hub is unreleased.")
