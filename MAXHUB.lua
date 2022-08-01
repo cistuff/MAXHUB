68b6db6436159955accd89df94b65fa3726ec08f
cistuff <89096213+cistuff@users.noreply.github.com>
2022-08-01 12:22:16 -0400

    Update MAXHUB.lua

--- MAXHUB.lua
+++ MAXHUB.lua
@@ -7,7 +7,8 @@
 ██║░╚═╝░██║██║░░██║██╔╝╚██╗██║░░██║╚██████╔╝██████╦╝
 ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═════╝░
 
-Max Hub is not ready yet! The individual scripts for the games that Max Hub support are located in the Max Hub Custom folder in this repo. 
+SCRIPTS FOR INDIVIDUAL GAMES SUCH AS JTOH CAN BE FOUND IN Scripts.lua!
+
+This version of Max Hub is unreleased.
 
 ]]
-print("This version of Max Hub is unreleased.")
